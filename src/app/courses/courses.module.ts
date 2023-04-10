94c4870d87ffa50b4a56da48cf64df184ad5935c
Gabriel-Nunes-dev <gabnunesdev@gmail.com>
2023-04-10 19:50:39 -0300

    Init

---
+++ src/app/courses/courses.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { CoursesComponent } from './courses/courses.component';
+import { CoursesRoutingModule } from './courses-routing.module';
+
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
+
+
+@NgModule({
+  declarations: [
+    CoursesComponent
+  ],
+  imports: [
+    CommonModule,
+    CoursesRoutingModule,
+    AppMaterialModule
+  ]
+})
+export class CoursesModule { }
